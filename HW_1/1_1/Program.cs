a6cc963711a342facad696bac62fad55fe7d5935
марта <kukovamk@gmail.com>
2023-03-04 23:03:54 +0300

    Task 1_1

---
+++ HW_1/1_1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдает, какое число большее, а какое меньшее.
+
+string n_1 = Console.ReadLine()!;
+string n_2 = Console.ReadLine()!;
+
+int a = int.Parse(n_1);
+int b = int.Parse(n_2);
+
+
+if (a > b)
+
+Console.WriteLine( a > b );
+
+else if (a == b)
+
+Console.WriteLine(a = b);
+
+else if (a < b)
+
+Console.WriteLine(a < b);
+
