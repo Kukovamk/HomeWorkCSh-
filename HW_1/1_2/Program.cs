ba165e9908690e54c9e29c07d6a52ad2cd9df0c2
марта <kukovamk@gmail.com>
2023-03-04 23:47:12 +0300

    Task 1_2

---
+++ HW_1/1_2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдает максимальное из этих чисел.
+
+int n_1 = int.Parse(Console.ReadLine()!);
+int n_2 = int.Parse(Console.ReadLine()!);
+int n_3 = int.Parse(Console.ReadLine()!);
+
+
+if (n_1 < n_2)
+{
+    n_1 = n_2;
+}
+if (n_1 < n_3)
+{
+    n_1 = n_3;
+}
+Console.Write($"The number {n_1} max");
+
+
+
+
+
